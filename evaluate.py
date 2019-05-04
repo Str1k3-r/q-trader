5a199e3d38b070a169cfc20e0b5ef3f4d87cfe8e
striker <prateektank120@gmail.com>
2019-05-04 19:52:07 -0400

    "Changed to Python3. Added matplotlib"

--- evaluate.py
+++ evaluate.py
@@ -4,9 +4,10 @@ from keras.models import load_model
 from agent.agent import Agent
 from functions import *
 import sys
+import matplotlib.pyplot as plt
 
 if len(sys.argv) != 3:
-	print "Usage: python evaluate.py [stock] [model]"
+	print("Usage: python evaluate.py [stock] [model]")
 	exit()
 
 stock_name, model_name = sys.argv[1], sys.argv[2]
@@ -21,8 +22,10 @@ batch_size = 32
 state = getState(data, 0, window_size + 1)
 total_profit = 0
 agent.inventory = []
+actions = []
+total_profit_graph = []
 
-for t in xrange(l):
+for t in range(l):
 	action = agent.act(state)
 
 	# sit
@@ -30,20 +33,30 @@ for t in xrange(l):
 	reward = 0
 
 	if action == 1: # buy
+		actions.append(("BUY", data[t]))
 		agent.inventory.append(data[t])
-		print "Buy: " + formatPrice(data[t])
+		print("Buy: " + formatPrice(data[t]))
 
 	elif action == 2 and len(agent.inventory) > 0: # sell
+		actions.append(("SELL", data[t]))
 		bought_price = agent.inventory.pop(0)
 		reward = max(data[t] - bought_price, 0)
 		total_profit += data[t] - bought_price
-		print "Sell: " + formatPrice(data[t]) + " | Profit: " + formatPrice(data[t] - bought_price)
+		print("Sell: " + formatPrice(data[t]) + " | Profit: " + formatPrice(data[t] - bought_price))
+		total_profit_graph.append(total_profit)
 
 	done = True if t == l - 1 else False
 	agent.memory.append((state, action, reward, next_state, done))
 	state = next_state
 
 	if done:
-		print "--------------------------------"
-		print stock_name + " Total Profit: " + formatPrice(total_profit)
-		print "--------------------------------"
+		x = [i for i in range(len(total_profit_graph))]
+		plt.plot(x, total_profit_graph)
+		plt.title("Total Profits " + stock_name + "_" + model_name)
+		plt.legend()
+		plt.show()
+		plt.savefig("tp_" + stock_name + "_" + model_name + ".png")
+
+		print("--------------------------------")
+		print(stock_name + " Total Profit: " + formatPrice(total_profit))
+		print("--------------------------------")
--- train.py
+++ train.py
@@ -1,9 +1,11 @@
 from agent.agent import Agent
 from functions import *
 import sys
+import numpy as np
+import random
 
 if len(sys.argv) != 4:
-	print "Usage: python train.py [stock] [window] [episodes]"
+	print("Usage: python train.py [stock] [window] [episodes]")
 	exit()
 
 stock_name, window_size, episode_count = sys.argv[1], int(sys.argv[2]), int(sys.argv[3])
@@ -12,16 +14,22 @@ agent = Agent(window_size)
 data = getStockDataVec(stock_name)
 l = len(data) - 1
 batch_size = 32
+epsilon = 0.05
 
-for e in xrange(episode_count + 1):
-	print "Episode " + str(e) + "/" + str(episode_count)
+for e in range(episode_count + 1):
+	print("Episode " + str(e) + "/" + str(episode_count))
 	state = getState(data, 0, window_size + 1)
 
 	total_profit = 0
 	agent.inventory = []
 
-	for t in xrange(l):
-		action = agent.act(state)
+	for t in range(l):
+
+		if (np.random.uniform(0, 0.1) < epsilon):
+			action = random.randint(1, 3)
+			epsilon /= t
+		else:
+			action = agent.act(state)
 
 		# sit
 		next_state = getState(data, t + 1, window_size + 1)
@@ -29,22 +37,22 @@ for e in xrange(episode_count + 1):
 
 		if action == 1: # buy
 			agent.inventory.append(data[t])
-			print "Buy: " + formatPrice(data[t])
+			print("Buy: " + formatPrice(data[t]))
 
 		elif action == 2 and len(agent.inventory) > 0: # sell
 			bought_price = agent.inventory.pop(0)
 			reward = max(data[t] - bought_price, 0)
 			total_profit += data[t] - bought_price
-			print "Sell: " + formatPrice(data[t]) + " | Profit: " + formatPrice(data[t] - bought_price)
+			print("Sell: " + formatPrice(data[t]) + " | Profit: " + formatPrice(data[t] - bought_price))
 
 		done = True if t == l - 1 else False
 		agent.memory.append((state, action, reward, next_state, done))
 		state = next_state
 
 		if done:
-			print "--------------------------------"
-			print "Total Profit: " + formatPrice(total_profit)
-			print "--------------------------------"
+			print("--------------------------------")
+			print("Total Profit: " + formatPrice(total_profit))
+			print("--------------------------------")
 
 		if len(agent.memory) > batch_size:
 			agent.expReplay(batch_size)
