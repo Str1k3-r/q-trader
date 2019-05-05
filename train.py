174b8b947e580bfc0987bf974e3f2f6edaf6e7a1
striker <prateektank120@gmail.com>
2019-05-04 20:07:26 -0400

    "Added total profit per episode for training"

--- train.py
+++ train.py
@@ -5,8 +5,8 @@ import numpy as np
 import random
 
 if len(sys.argv) != 4:
-	print("Usage: python train.py [stock] [window] [episodes]")
-	exit()
+    print("Usage: python train.py [stock] [window] [episodes]")
+    exit()
 
 stock_name, window_size, episode_count = sys.argv[1], int(sys.argv[2]), int(sys.argv[3])
 
@@ -15,51 +15,49 @@ data = getStockDataVec(stock_name)
 l = len(data) - 1
 batch_size = 32
 epsilon = 0.05
-tp_per_episode = []
+tp_array = []
 for e in range(episode_count + 1):
-	print("Episode " + str(e) + "/" + str(episode_count))
-	state = getState(data, 0, window_size + 1)
+    print("Episode " + str(e) + "/" + str(episode_count))
+    state = getState(data, 0, window_size + 1)
 
-	total_profit = 0
-	agent.inventory = []
+    total_profit = 0
+    agent.inventory = []
 
-	for t in range(l):
+    for t in range(l):
+        if (np.random.uniform(0, 0.1) < epsilon):
+            action = np.random.randint(1, 3)
+        else:
+            action = agent.act(state)
 
-		if (np.random.uniform(0, 0.1) < epsilon):
-			action = random.randint(1, 3)
-            epsilon /= e
-		else:
-			action = agent.act(state)
+        # sit
+        next_state = getState(data, t + 1, window_size + 1)
+        reward = 0
 
-		# sit
-		next_state = getState(data, t + 1, window_size + 1)
-		reward = 0
+        if action == 1:  # buy00
+            agent.inventory.append(data[t])
+            print("Buy: " + formatPrice(data[t]))
 
-		if action == 1: # buy
-			agent.inventory.append(data[t])
-			print("Buy: " + formatPrice(data[t]))
+        elif action == 2 and len(agent.inventory) > 0:  # sell
+            bought_price = agent.inventory.pop(0)
+            reward = max(data[t] - bought_price, 0)
+            total_profit += data[t] - bought_price
+            print("Sell: " + formatPrice(data[t]) + " | Profit: " + formatPrice(data[t] - bought_price))
 
-		elif action == 2 and len(agent.inventory) > 0: # sell
-			bought_price = agent.inventory.pop(0)
-			reward = max(data[t] - bought_price, 0)
-			total_profit += data[t] - bought_price
-			print("Sell: " + formatPrice(data[t]) + " | Profit: " + formatPrice(data[t] - bought_price))
+        done = True if t == l - 1 else False
+        agent.memory.append((state, action, reward, next_state, done))
+        state = next_state
 
-		done = True if t == l - 1 else False
-		agent.memory.append((state, action, reward, next_state, done))
-		state = next_state
+        if done:
+            print("--------------------------------")
+            print("Total Profit: " + formatPrice(total_profit))
+            print("--------------------------------")
+            tp_array.append(total_profit)
 
-		if done:
-			print("--------------------------------")
-			print("Total Profit: " + formatPrice(total_profit))
-			print("--------------------------------")
-            tp_per_episode.append(total_profit)
+        if len(agent.memory) > batch_size:
+            agent.expReplay(batch_size)
 
-		if len(agent.memory) > batch_size:
-			agent.expReplay(batch_size)
+    if e % 10 == 0:
+        agent.model.save("models/model_ep" + str(e))
 
-	if e % 10 == 0:
-		agent.model.save("models/model_ep" + str(e))
-
-for tp in tp_per_episode:
+for tp in tp_array:
     print(tp)
